c9e1a3a1d429f39491db243fc17225d45b2c3049
ljun20160606 <ljun20160606@gmail.com>
2020-03-29 12:56:24 +0800

    test(version_test.go): test `sshw version new`

---
+++ cmd/sshw/version_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"os/exec"
+	"testing"
+)
+
+func Test_versionNewCmd(t *testing.T) {
+	ast := assert.New(t)
+	showVersionCmd := exec.Command("sshw", "version", "new")
+	output, err := showVersionCmd.Output()
+	ast.Nil(err)
+	fmt.Println(string(output))
+}
